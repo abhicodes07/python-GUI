67b1fc3dcefea184bd021b0f31c144d9106862ee
Abhinav <abhiasthana76@gmail.com>
2024-10-18 13:06:08 +0530

    GUI

---
+++ 01_hello.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+# TODO: first thing to do 
+# create a object of tkinter classs
+root = Tk()
+
+# TODO: Create a widget
+# creating a myLable widget
+myLable = Label(root, text="Hello GUI!")
+
+# TODO: Pack things up
+# showing on the screen
+myLable.pack()
+
+root.mainloop()
